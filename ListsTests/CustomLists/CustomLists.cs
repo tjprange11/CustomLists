1d61c4e05e4fda4c15d58c804e9e354f12f017ba
TJ Prange <tjprange11@gmail.com>
2018-04-09 08:43:01 -0500

    2 Removed method tests

---
+++ ListsTests/CustomLists/CustomLists.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomLists
+{
+    class CustomLists
+    {
+        //memeber variables
+        T[] array = new T[20];
+        //constructor
+
+        //member methods
+    }
+}
--- ListsTests/ListsTests/UnitTest1.cs
+++ ListsTests/ListsTests/UnitTest1.cs
@@ -12,24 +12,58 @@ namespace ListsTests
         {
             //Arrange
             List<int> nums = new List<int>() { 1, 2, 3 };
-            List<int> nums2 = new List<int>() { 0, 1, 2, 3 };
             int number = 0;
             //Act
             nums.Add(number);
             //Assert
-            Assert.AreNotEqual(nums[0],nums2[0]);
+            Assert.AreNotEqual(nums[0],number);
         }
         [TestMethod]
         public void addElement_CheckElementIsAddedToBack()
         {
             //Arrange
             List<int> nums = new List<int>() { 0, 1, 2};
-            List<int> nums2 = new List<int>() { 0, 1, 2, 3 };
             int number = 3;
             //Act
             nums.Add(number);
             //Assert
-            Assert.AreEqual(nums[0], nums2[0]);
+            Assert.AreEqual(nums[nums.Count - 1], number);
+        }
+        [TestMethod]
+        public void removeElement_CheckElementIsRemoved()
+        {
+            //Arrange
+            List<int> nums = new List<int>() { 1, 2, 3 };
+            //Act
+            nums.Remove(2);
+            //Assert
+            bool removed = true;
+            for (int i = 0; i < nums.Count; i++)
+            {
+                if (nums[i] == 2)
+                {
+                    removed = false;
+                }
+            }
+            Assert.IsTrue(removed);
+        }
+        [TestMethod]
+        public void removeElement_CheckIfAllElementsAreRemoved()
+        {
+            //Arrange
+            List<int> nums = new List<int>() { 1, 2, 2, 3 };
+            //Act
+            nums.Remove(2);
+            //Assert
+            bool removed = true;
+            for (int i = 0; i < nums.Count; i++)
+            {
+                if (nums[i] == 2)
+                {
+                    removed = false;
+                }
+            }
+            Assert.IsFalse(removed);
         }
     }
 }
